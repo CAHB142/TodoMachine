6a25856c698a157b1f85abec7dfa5de728fb333f
Carlos Henao <carlosenaob@gmail.com>
2024-08-29 10:25:21 -0500

    se agrego el completar y eliminar todos

--- src/App.js
+++ src/App.js
@@ -32,6 +32,23 @@ function App() {
       return todoText.includes(searchText);
         }
   );
+  const completeTodo = (text) =>{
+     const newTodos=[...todos];
+     const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+     );
+     newTodos[todoIndex].completed =true;
+     setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) =>{
+    const newTodos=[...todos];
+    const todoIndex = newTodos.findIndex(
+     (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+ };
 
   return (
     <>
@@ -43,7 +60,10 @@ function App() {
         {searchedTodos.map(todo => (
           <TodoItem key={todo.text} 
           text={todo.text}
-          completed ={todo.completed}/>
+          completed ={todo.completed}
+          onComplete={() => completeTodo(todo.text)}
+          onDelete={() => deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
 
