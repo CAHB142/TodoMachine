22c9d7e69ff709241ac381abeef389e37a7c3a71
Carlos Henao <carlosenaob@gmail.com>
2024-09-03 18:30:38 -0500

    Se agrego colores dinamidos

--- src/js/TodoIcon.js
+++ src/js/TodoIcon.js
@@ -1,14 +1,16 @@
 import {ReactComponent as CheckSVG}  from './check.svg'
 import {ReactComponent as DeleteSVG}  from './delete.svg'
+import '../css/TodoIcon.css';
 
 const IconTypes={
-    "check":<CheckSVG/>,
-    "delete":<DeleteSVG/>
+    "check":(color) =><CheckSVG className="Icon-svg" fill={color}/>,
+    "delete": (color) =><DeleteSVG className="Icon-svg" fill={color}/>
 }
-function TodoIcon({type}){
+function TodoIcon({type, color, onClick}){
     return (
-        <span class={`Icon Icon-svg Icon-${type}`}>
-         {IconTypes[type]}
+        <span class={`Icon-container Icon-container-${type}`} onClick={onClick}>
+         {IconTypes[type](color)}
+         
         </span>
     );
 }
